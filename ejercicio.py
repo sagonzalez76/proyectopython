964f0a740396f5aef1d7355af7469d57d588ba66
Sory Gonzalez <sagonzalez76@misena.edu.co>
2022-12-01 10:51:44 -0500

    IF

--- ejercicio.py
+++ ejercicio.py
@@ -1,10 +1,8 @@
 edad = int(input("Ingrese la edad: "))
 
-if edad <= 25:
+if edad < 26:
     print("Eres estudiante")
-
-elif edad > 25 & edad <= 65:
+elif edad > 25 and edad < 66:
     print("Eres profesor")
-
 elif edad > 65:
     print("Eres Master")
